692944353809231b6a7b09e76020a1c9bf1097e4
FredWalls <fwalls@m.smpte.org>
2026-07-14 17:58:09 -0500

    Regenerated reference data after bug fix

--- refs/ctest_fp16_RGB444_lsbf_l2r_norle.0.r
+++ refs/ctest_fp16_RGB444_lsbf_l2r_norle.0.r
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9c6c1f848ee61c8075086e08ee3a79cd919d0933a886020525f757a3a77782b
+oid sha256:dd5dc7d7320fd2cc99a2d7d07dc2f70cfed276eae2827a0089cca7094a53e3a0
 size 4147200
--- refs/ctest_fp16_RGB444_msbf_l2r_norle.0.g
+++ refs/ctest_fp16_RGB444_msbf_l2r_norle.0.g
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e787e8c5fbc45dd1ecf861717a51e78fa8322980d171169dd9519d3f743154aa
+oid sha256:92021f4fe7831aed319592227cdfb4eded15e7c8315daf9ce24679020f5d2990
 size 4147200
